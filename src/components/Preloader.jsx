3fb37b5d1dde753e7fdfd441d40c46a6aeb87180
gowtham krishna p <gowthamkrishnap006@gmail.com>
2025-11-24 12:59:15 +0530

    initial commit

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Preloader() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYkDNxH61w2dkVRTRESvfFWtZW2AIEOIWzsg&s" alt="" />
+    </div>
+  )
+}
+
+export default Preloader
